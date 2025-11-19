233efa6c262577c333f0925d30c9d07f916e200e
Emilla Rose <emillarose028@gmail.com>
2025-11-20 07:58:16 +0800

    Create List.py

---
+++ DSA in Python/List.py
@@ -0,0 +1,33 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        temp = self.head
+        while temp.next:
+            temp = temp.next
+        temp.next = new_node
+
+    def display(self):
+        temp = self.head
+        while temp:
+            print(temp.data, end=" -> ")
+            temp = temp.next
+        print("None")
+
+node = LinkedList()
+node.append(10)
+node.append(20)
+node.append(30)
+
+print("Linked List:")
+node.display()
