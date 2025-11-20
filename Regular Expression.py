b36903f8056b165c6e300326e4e02ccaf006da2c
Emilla Rose <emillarose028@gmail.com>
2025-11-20 09:06:26 +0800

    Create Regular Expression.py

---
+++ Regular Expression.py
@@ -0,0 +1,10 @@
+import re
+
+email = input("Enter your email: ")
+
+pattern = r'^[\w\.-]+@[\w\.-]+\.\w+$'
+
+if re.match(pattern, email):
+    print("Valid email!")
+else:
+    print("Invalid email.")
