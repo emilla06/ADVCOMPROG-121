43481ee56dc188e5b6d7c0c335c2aa6d1ff9d241
Emilla Rose <emillarose028@gmail.com>
2025-11-20 08:17:48 +0800

    Update Inheritance.py

--- Concept of OOP/Inheritance.py
+++ Concept of OOP/Inheritance.py
@@ -1,3 +1,4 @@
+#Animal Inheritance
 class Animal:
     def speak(self, speak):
         print("their sounds")
@@ -12,3 +13,28 @@ class Cat(Animal):
 
 Dog().speak()
 Cat().speak()
+
+
+
+#Vehicle Class
+class Vehicle:
+    def __init__(self, brand, fuel):
+        self.brand = brand
+        self.fuel = fuel
+
+class Car(Vehicle):
+    def __init__(self, brand, fuel, doors):
+        Vehicle.__init__(self, brand, fuel)
+        self.doors = doors
+
+    def drive(self):
+        if self.fuel > 0:
+            self.fuel -= 1
+            print(f"The {self.brand} car is driving... Fuel left: {self.fuel}")
+        else:
+            print(f"The {self.brand} car has no fuel left!")
+
+my_car = Car("Ford", 5, 4)
+my_car.drive()
+my_car.drive()
+my_car.drive()
