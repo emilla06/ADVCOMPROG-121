c944226c6682ec124794e15423a780f321b94b2d
Emilla Rose <emillarose028@gmail.com>
2025-11-20 08:21:15 +0800

    Update Polymorphism.py

--- Concept of OOP/Polymorphism.py
+++ Concept of OOP/Polymorphism.py
@@ -1,3 +1,4 @@
+#Printer
 class InkPrinter:
     def print_document(self):
         return "Printing using ink..."
@@ -9,3 +10,20 @@ class LaserPrinter:
 printers = [InkPrinter(), LaserPrinter()]
 for prnt in printers:
     print(prnt.print_document())
+
+
+
+#Duck Typing
+def make_it_speak(animal):
+    animal.speak()
+
+class Bird:
+    def speak(self):
+        print("Chirp! Chirp!")
+
+class Robot:
+    def speak(self):
+        print("Beep! boop!")
+
+make_it_speak(Bird())
+make_it_speak(Robot())
